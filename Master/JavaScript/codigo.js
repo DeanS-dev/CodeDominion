188215c64113819a32db91c72a25b76ff2e6886f
DeanS-dev <danielcuestav@hotmail.com>
2024-11-22 23:10:26 -0400

    pagina Guerra

---
+++ Master/JavaScript/codigo.js
@@ -0,0 +1,7 @@
+//alert("FELICIDADES!!!");
+//console.log("hola mundo");
+//console.warn("cuidado hay un maco en la pagina")
+//console.error("hay un maco en verdad")
+
+var edad=10;
+console.log("10 por 2 es:"+(edad*2))
